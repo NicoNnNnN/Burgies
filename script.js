0e40372b4424ce0f7db720cfe3a49c2d8415d74e
NicoNnNnN <niconuscheler99@gmail.com>
2025-05-02 11:07:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function checkPassword() {
     const password = document.getElementById("passwordInput").value;
-    const message = document.getElementById("statusMessage");
 
     if (password === "HellfireFLN2025") {
-        message.innerText = "Zugang erlaubt! (Hier beginnt deine Webapp)";
-        message.style.color = "green";
+        // Weiterleitung auf neue Seite
+        window.location.href = "home.html";
     } else {
+        const message = document.getElementById("statusMessage");
         message.innerText = "Falsches Passwort!";
         message.style.color = "red";
     }
