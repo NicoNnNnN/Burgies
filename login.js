e57ba1f8c4bf6b02459c3439a14f67c43f521f22
NicoNnNnN <niconuscheler99@gmail.com>
2025-05-07 23:24:11 +0200

    Delete login.js

--- login.js
+++
@@ -1,9 +0,0 @@
-export function checkPassword() {
-  const pw = document.getElementById("password").value;
-  if (pw === "HellfireFLN2025") {
-    window.location.href = "home.html";
-  } else {
-    alert("Falsches Passwort!");
-  }
-}
-window.checkPassword = checkPassword;
